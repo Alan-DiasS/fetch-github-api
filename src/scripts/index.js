ade4399ea0dd9ee1409f85c97da042333c4c8754
Alan Dias <alandiasdos598@gmail.com>
2025-03-19 13:33:40 -0300

    caminhos relativos

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,7 @@
-import { getUser } from '/src/scripts/services/user.js';
-import { respositories } from "/src/scripts/services/respositories.js";
-import {user} from "/src/scripts/services/objects/user.js";
-import {screen} from "/src/scripts/services/objects/screen.js";
+import { getUser } from './services/user.js';
+import { respositories } from "./services/repositories.js";
+import {user} from "./services/objects/user.js";
+import {screen} from "./services/objects/screen.js";
 
 document.getElementById("btn-search").addEventListener("click", () => {
   const userName = document.getElementById("input-search").value
